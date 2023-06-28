0865e1e592919df7d05832f9ab3a4f32902d3fea
Evgenia Stepanova <evgeniast85es@gmail.com>
2023-06-28 11:33:20 +0300

    feat: connect styles

---
+++ src/app/assets/index.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+import './styles/index.css';
+import Tasks from '../../pages/Tasks';
+
+const App: FC = () => (
+  <Tasks/>
+)
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/styles/index.css';
-import Tasks from './pages/Tasks';
+import App from './app/assets';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Tasks />
-  </React.StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
